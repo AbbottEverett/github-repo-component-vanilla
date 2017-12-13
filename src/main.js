3600a9e8a3274f556c11f041955d5cff18870b6c
S. Everett Abbott <severettabbott@gmail.com>
2017-12-13 15:48:50 -0800

    Added additional styles, refactored the repo class

--- src/main.js
+++ src/main.js
@@ -22,25 +22,72 @@ function loadAllRepos(apiUrl, user, array, callback) {
 class Repo {
   constructor(repoData, id) {
     this.id = id;
+    this.fork = repoData.fork;
     this.name = repoData.name;
     this.language = repoData.language;
     this.html_url = repoData.html_url;
-    this.fork = repoData.fork;
     this.star_count = repoData.stargazers_count;
     this.watchers = repoData.watchers_count;
-    this.description = repoData.description;
     this.forks_count = repoData.forks_count;
+    this.description = repoData.description;
   }
-  render() {
+  createElement(tag, textContent, classList) {
+    let el = document.createElement(tag);
+    if (textContent !== undefined) {
+      el.textContent = textContent;
+    }
+    if (classList !== undefined) {
+      el.classList = classList;
+    }
+    return el;
+  }
+  createIcon(classNames) {
+    let icon = document.createElement('i');
+    icon.classList = classNames;
+    return icon;
+  }
+  renderRepo() {
     let repo = document.createDocumentFragment();
-    let div = document.createElement('div');
-    div.classList.add('repo');
+    let div = this.createElement('div', undefined, 'repo');
+    let ul = this.createElement('ul');
     repo.append(div);
-    let ul = document.createElement('ul');
     div.append(ul);
-    Object.keys(this).forEach((key) =>{
-      let li = document.createElement('li');
-      li.textContent = key + ': ' + this[key];
+    Object.keys(this).forEach((key, i) => {
+      let li = this.createElement('li');
+      switch(key) {
+        case 'fork':
+          if(this[key]) {
+            li.append(this.createIcon('fa fa-code-fork'));
+          } else {
+            li.append(this.createIcon('fa fa-book'));
+          }
+          break;
+        case 'name':
+          let header = this.createElement('h2');
+          let link = this.createElement('a', this[key]);
+          link.setAttribute('href', this.html_url);
+          header.append(link);
+          li.append(header)
+          li.classList = 'header';
+          break;
+        case 'language':
+          if (this[key]) {
+            li.append(this.createElement('h3', this[key]));
+          }
+          break;
+        case 'forks_count':
+        case 'watchers':
+        case 'star_count':
+          li.append(this.createIcon('fa fa-star'));
+          li.append(this.createElement('span', this[key]));
+          break;
+        case 'html_url':
+        case 'id':
+          break;
+        default:
+          li.textContent = this[key];
+          break;
+      }
       ul.append(li);
     });
     return repo;
@@ -56,7 +103,7 @@ function renderRepos(repoList) {
   div.id = "repoList";
   root.append(div);
   repoList.forEach((repo) => {
-    div.append(repo.render());
+    div.append(repo.renderRepo());
   });
 }
 
